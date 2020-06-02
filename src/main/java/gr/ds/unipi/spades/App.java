5074c2d953ef07c37895024cc228d12e7826151d
IoannisTsirovasilis <ioannistsirovasilis@gmail.com>
2020-06-02 11:45:50 +0300

    Initial Commit

---
+++ src/main/java/gr/ds/unipi/spades/App.java
@@ -0,0 +1,13 @@
+package gr.ds.unipi.spades;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
