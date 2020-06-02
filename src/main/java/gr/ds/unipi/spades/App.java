25c9740f2895f18cb9ede7021b635316028c8098
Ioannis Tsirovasilis <ioannistsirovasilis@gmail.com>
2020-06-02 14:00:55 +0300

    Initial commit for applying spatial index

--- src/main/java/gr/ds/unipi/spades/App.java
+++ src/main/java/gr/ds/unipi/spades/App.java
@@ -1,7 +1,10 @@
 package gr.ds.unipi.spades;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import gr.ds.unipi.irregularGrid.IrregularGrid;
+import gr.ds.unipi.irregularGrid.Point;
 
 /**
  * Hello world!
@@ -9,9 +12,18 @@ import org.apache.spark.api.java.JavaSparkContext;
  */
 public class App 
 {
+	private static final String FILE_PATH = "hdfs://localhost:9000/user/test/";
     public static void main( String[] args )
     {
+    	int lonIndex = 5;
+    	int latIndex = 4;
+    	
         SparkConf conf = new SparkConf().setMaster("local").setAppName("Test Spark");
         JavaSparkContext sc = new JavaSparkContext(conf);
+        System.out.println("Reading file...");
+        JavaRDD<String> input = sc.textFile(FILE_PATH + "hotels-ver1.txt");
+        System.out.println("File read.");
+        System.out.println(input.first());
+        
     }
 }
